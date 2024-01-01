c70e10629c84212cd358b1a4bae615ad81113965
Rodrigo Santos <rodrigom376@gmail.com>
2024-01-01 16:46:36 -0300

    adicionado component navbar, logo, bootsatrap

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+// Import necessary Angular modules and components
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component'; // Update the path
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [], // Add services if needed
+  bootstrap: [AppComponent], // Specify the root component
+})
+export class AppModule {}

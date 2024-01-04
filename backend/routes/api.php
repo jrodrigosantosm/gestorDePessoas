b390f73fcef808b6d16748305eff7f083f6de109
Rodrigo Santos <rodrigom376@gmail.com>
2024-01-03 23:45:37 -0300

    api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::apiResource('/users', UserController::class);
+// Route::delete('/users/{id}', [UserController::class, 'destroy']);
+// Route::patch('/users/{id}', [UserController::class, 'update']);
+// Route::get('/users/{id}', [UserController::class, 'show']);
+// Route::get('/users', [UserController::class, 'index']);
+// Route::post('/users', [UserController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return response()->json([
+        'success' => true
+    ]);
 });
